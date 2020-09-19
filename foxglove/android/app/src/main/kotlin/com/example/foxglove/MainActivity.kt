eed3b21a90077aeedc6c315e2987216d135c9a36
Kolja Esders <kolja.esders@gmail.com>
2020-09-19 14:30:20 +0200

    LETS GO (2)

---
+++ foxglove/android/app/src/main/kotlin/com/example/foxglove/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.foxglove
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lets-go/README.md
+++
@@ -1,2 +0,0 @@
-Kotlin ARCore Sample App
-========================
