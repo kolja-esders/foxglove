8ed7f978c9bf91c6b838501a6922df95410ac3e4
Oruluk <pat.dein@gmail.com>
2020-09-20 01:56:46 +0200

    Removed surplus package

--- foxglove/lib/pages/evaluation_page.dart
+++ foxglove/lib/pages/evaluation_page.dart
@@ -1,6 +1,6 @@
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_emoji/flutter_emoji.dart';
+
 
 class EvaluationPage extends StatelessWidget {
   EvaluationPage(this.args)
