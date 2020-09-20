e0f3b3389fc9f7d4d16e8c7d78e23dedbe08419b
dzimmerer <d.zimmerer@gmail.com>
2020-09-20 03:49:05 +0200

    Add images

--- foxglove/lib/pages/evaluation_page.dart
+++ foxglove/lib/pages/evaluation_page.dart
@@ -21,7 +21,6 @@ class EvaluationPage extends StatelessWidget {
         'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/263/sake_1f376.png',
     'salt':
         'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/263/salt_1f9c2.pnggg',
-    'pepper': ''
   };
 
   @override
